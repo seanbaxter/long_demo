62d665650f0b2838c14cf1a1211887663f99958e
Sean Baxter <lightborn@gmail.com>
2019-11-27 21:54:49 -0500

    Changed tabs to spaces

--- qsort.cxx
+++ qsort.cxx
@@ -5,40 +5,40 @@
 
 template<typename type_t>
 int cmp_pred(const void* a, const void* b) {
-	type_t x = *static_cast<const type_t*>(a);
-	type_t y = *static_cast<const type_t*>(b);
-	return x < y ? -1 : x > y ? +1 : 0;
+  type_t x = *static_cast<const type_t*>(a);
+  type_t y = *static_cast<const type_t*>(b);
+  return x < y ? -1 : x > y ? +1 : 0;
 }
 
 template<typename type_t>
 void sort_vec(std::vector<type_t>& vec) {
-	// cmp_pred<type_t> returns the address of an FFI closure here.
-	// The Circle interpreter makes a foreign function call to 
-	// qsort, and qsort invokes the closure. The interpreter,
-	// which implements the closure, regains control, and executes
-	// cmp_pred by traversing its AST.
-	qsort(vec.data(), vec.size(), sizeof(type_t), cmp_pred<type_t>);
+  // cmp_pred<type_t> returns the address of an FFI closure here.
+  // The Circle interpreter makes a foreign function call to 
+  // qsort, and qsort invokes the closure. The interpreter,
+  // which implements the closure, regains control, and executes
+  // cmp_pred by traversing its AST.
+  qsort(vec.data(), vec.size(), sizeof(type_t), cmp_pred<type_t>);
 }
 
 template<typename type_t>
 void sort_test(size_t count, bool verbose = false) {
-	std::vector<type_t> vec(count);
-	for(size_t i = 0; i < count; ++i)
-		vec[i] = rand() % 1000;
+  std::vector<type_t> vec(count);
+  for(size_t i = 0; i < count; ++i)
+    vec[i] = rand() % 1000;
 
-	sort_vec(vec);
-	bool sorted = std::is_sorted(vec.begin(), vec.end());
-	if(!sorted) {
-		fputs("input array not sorted", stderr);
+  sort_vec(vec);
+  bool sorted = std::is_sorted(vec.begin(), vec.end());
+  if(!sorted) {
+    fputs("input array not sorted", stderr);
 
-	} else if(verbose) {
-		for(auto x : vec)
-			std::cout<< x<< "\n";
-	}
+  } else if(verbose) {
+    for(auto x : vec)
+      std::cout<< x<< "\n";
+  }
 }
 
 int main() {
-	@meta sort_test<float>(50, true);
+  @meta sort_test<float>(50, true);
 
-	return 0;
+  return 0;
 }
